fb4c4cc14629fae2640cb65e0f2486738526761b
Pavel Byles <pavz@MBQTQ693.jncb.com>
2022-10-30 20:01:42 -0500

    Added folder stucture for more webservices and apps

---
+++ app_api/src/settings.rs
@@ -0,0 +1,67 @@
+use std::fmt;
+
+use config::{Config, ConfigError, File};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Server {
+    pub host: String,
+    pub port: u16,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Log {
+    pub level: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Settings {
+    pub server: Server,
+    pub log: Log,
+    pub env: ENV,
+}
+
+const CONFIG_FILE_PATH: &str = "./config/Default.toml";
+const CONFIG_FILE_PREFIX: &str = "./config/";
+
+#[derive(Clone, Debug, Deserialize)]
+pub enum ENV {
+    Development,
+    Testing,
+    Production,
+}
+
+impl fmt::Display for ENV {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ENV::Development => write!(f, "Development"),
+            ENV::Testing => write!(f, "Testing"),
+            ENV::Production => write!(f, "Production"),
+        }
+    }
+}
+
+impl From<&str> for ENV {
+    fn from(env: &str) -> Self {
+        match env {
+            "Testing" => ENV::Testing,
+            "Production" => ENV::Production,
+            _ => ENV::Development,
+        }
+    }
+}
+
+impl Settings {
+    pub fn new() -> Result<Self, ConfigError> {
+        let env = std::env::var("RUN_ENV").unwrap_or_else(|_| "Development".into());
+        let s = Config::builder()
+            .set_override("env", env.clone())?
+            .add_source(File::with_name(CONFIG_FILE_PATH))
+            .add_source(File::with_name(&format!("{}{}", CONFIG_FILE_PREFIX, env)))
+            // This makes it so "EA_SERVER__PORT overrides server.port
+            .add_source(config::Environment::with_prefix("ea").separator("__"))
+            .build()?;
+
+        s.try_deserialize()
+    }
+}
