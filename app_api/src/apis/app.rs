8e3ff35d9c35425de4da5c626682aef081b30a05
Pavel Byles <pavz@MBQTQ693.jncb.com>
2022-11-29 18:59:34 -0500

    Fixed tests

--- app_api/src/apis/app.rs
+++ app_api/src/apis/app.rs
@@ -1,25 +1,42 @@
-use actix_web::{get, web};
-use serde::Serialize;
+use actix_web::{web, HttpRequest, HttpResponse};
+use serde::{Deserialize, Serialize};
+use std::str;
 
-#[derive(Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct HelloResponse {
     response: String,
 }
 
-#[get("/hello")]
-async fn greet_no_name() -> web::Json<HelloResponse> {
+pub async fn greet_no_name(_req: HttpRequest) -> HttpResponse {
     let resp = HelloResponse {
         response: "Hello World!".to_string(),
     };
 
-    web::Json(resp)
+    HttpResponse::Ok().json(resp)
 }
 
-#[get("/hello/{name}")]
-async fn greet_with_name(name: web::Path<String>) -> web::Json<HelloResponse> {
+pub async fn greet_with_name(_req: HttpRequest, name: web::Path<String>) -> HttpResponse {
     let resp = HelloResponse {
         response: format!("Hello {}!", name),
     };
 
-    web::Json(resp)
+    HttpResponse::Ok().json(resp)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{body::to_bytes, http::StatusCode, test};
+
+    #[actix_web::test]
+    async fn test_greet_no_name_ok() {
+        let req = test::TestRequest::default().to_http_request();
+        let http_resp = greet_no_name(req).await;
+
+        assert_eq!(http_resp.status(), StatusCode::OK);
+        let body_bytes = to_bytes(http_resp.into_body()).await.unwrap();
+        let hello_resp: HelloResponse =
+            serde_json::from_str(str::from_utf8(&body_bytes).unwrap()).unwrap();
+        assert_eq!(hello_resp.response, "Hello World!".to_string())
+    }
 }
--- app_api/src/apis/configuration.rs
+++ app_api/src/apis/configuration.rs
@@ -1,16 +1,14 @@
-use actix_web::{get, web};
-use serde::Serialize;
-use std::env;
+use actix_web::{HttpRequest, HttpResponse};
+use serde::{Deserialize, Serialize};
+use std::{env, str};
 
-#[derive(Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Config {
     target: String,
     port: u16,
 }
 
-#[get("/cfg")]
-async fn config() -> web::Json<Config> {
-    //let mut res_body = "Config vars: ".to_string();
+pub async fn config(_req: HttpRequest) -> HttpResponse {
     let cfg = Config {
         target: match env::var("TARGET") {
             Ok(env_target) => env_target,
@@ -19,5 +17,27 @@ async fn config() -> web::Json<Config> {
         port: 8080,
     };
 
-    web::Json(cfg)
+    HttpResponse::Ok().json(cfg)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{body::to_bytes, http::StatusCode, test};
+
+    #[actix_web::test]
+    async fn test_cfg_ok() {
+        env::set_var("TARGET", "test");
+
+        let req = test::TestRequest::default().to_http_request();
+        let http_resp = config(req).await;
+
+        assert_eq!(http_resp.status(), StatusCode::OK);
+
+        let body_bytes = to_bytes(http_resp.into_body()).await.unwrap();
+        let config_resp: Config =
+            serde_json::from_str(str::from_utf8(&body_bytes).unwrap()).unwrap();
+        assert_eq!(config_resp.target, "test".to_string());
+        assert_eq!(config_resp.port, 8080);
+    }
 }
