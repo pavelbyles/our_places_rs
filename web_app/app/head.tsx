c8006068d4ac6eff1c4e47ff5fd9e1957b6affe1
Pavel Byles <pavelbyles@gmail.com>
2023-05-13 15:53:33 -0500

    Updated project to GA version of NextJS13

--- web_app/app/head.tsx
+++ web_app/app/head.tsx
@@ -1,10 +1,10 @@
+import DefaultTags from './default-tags';
+
 export default function Head() {
   return (
     <>
-      <title>Our Places.io</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Villa and condo rental" />
-      <link rel="icon" href="/favicon.ico" />
+      <DefaultTags />
+      <meta name="test" content="aasdfsda" />
     </>
   )
 }
