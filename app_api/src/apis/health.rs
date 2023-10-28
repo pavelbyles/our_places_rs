6510a2a119bdff4d0b3e613f5307395c5fa47a40
Pavel Byles <pavelbyles@gmail.com>
2023-10-28 12:48:33 -0500

    Testing logging with target

--- app_api/src/apis/health.rs
+++ app_api/src/apis/health.rs
@@ -12,7 +12,7 @@ pub async fn health_check(_req: HttpRequest) -> HttpResponse {
         status: "alive".to_string(),
     };
 
-    log::trace!("Received request for: health_check");
+    log::info!(target: "root", "Received request for: health_check");
 
     HttpResponse::Ok().json(resp)
 }
