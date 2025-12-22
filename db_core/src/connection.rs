5152435e08d28913a26b4df821db01ba1babb5cc
Pavel Byles <pavelbyles@gmail.com>
2025-12-21 23:27:55 -0500

    Fixing build and deploy

--- db_core/src/connection.rs
+++ db_core/src/connection.rs
@@ -7,5 +7,9 @@ pub async fn create_connection_pool(db_url: &str) -> PgPool {
         .max_connections(5)
         .connect(db_url)
         .await
-        .expect("Failed to create database connection pool")
+        .unwrap_or_else(|e| {
+            tracing::error!("Failed to create database connection pool: {:?}", e);
+            std::thread::sleep(std::time::Duration::from_secs(2)); // Ensure logs flush
+            std::process::exit(1);
+        })
 }
