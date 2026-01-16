acc3c6928b44fc911bf12770e0bf4c56ffd26e6b
Pavel Byles <pavelbyles@gmail.com>
2026-01-16 01:28:42 -0500

    feat: Got Tailwind CSS and DaisyUI working #11

---
+++ web_app/src/components/shell.rs
@@ -0,0 +1,22 @@
+use crate::app::App;
+use leptos::prelude::*;
+use leptos_meta::*;
+
+#[component]
+pub fn AppShell(options: leptos::config::LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <AutoReload options=options.clone() />
+                <HydrationScripts options=options.clone()/>
+                <MetaTags/>
+            </head>
+            <body>
+                <App/>
+            </body>
+        </html>
+    }
+}
