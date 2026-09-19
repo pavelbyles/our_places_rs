1793370a80bbb89e02a3e2cf85c57447a1348b87
Pavel Byles <pavelbyles@gmail.com>
2026-09-19 16:53:01 -0500

    chore: introduce devenv developer environment, fix dev runner scripts, and patch rustls (#85)
    * Added devenv assets to project with some workflows
    
    * chore: update rustls to 0.23.45 to resolve RUSTSEC-2026-0285

--- scratch.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let hash = "$2y$12$N9uYmGgJd285q5sYd21OceA16Wq.4T2pG9jWc9qH9yQp0d3/QnLwe"; // example from htpasswd
-    let valid = bcrypt::verify("testpassword", hash).unwrap();
-    println!("Valid: {}", valid);
-}
